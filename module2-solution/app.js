c32307c1ee35d73e63c7d049b9f7d469aceab868
borovskayana <borovskayana7@gmail.com>
2022-08-25 02:05:50 +0200

    Module2.

---
+++ module2-solution/app.js
@@ -0,0 +1,99 @@
+
+(function () {
+    'use strict';
+
+  angular.module('ShoppingList', [])
+  .controller('ToBuyController',ToBuyController)
+  .controller('AlreadyBoughtController',AlreadyBoughtController)
+  .controller('AddtoList',AddtoList)
+  .service('ShoppingListService',ShoppingListService);
+
+  function ShoppingListService() {
+    var service = this ;
+
+
+    var addItems = [
+      {
+        name: "Milk",
+        quantity: "10"
+      },
+      {
+        name: "Cookies",
+        quantity: "10"
+      },
+      {
+        name: "Chocolate",
+        quantity: "10"
+      },
+      {
+        name: "Bounty",
+        quantity: "10"
+      },
+      {
+        name: "Cheese",
+        quantity: "10"
+      }
+    ];
+    var alreadyItems = [];
+    service.getaddItems = function functionName() {
+      return addItems ;
+    }
+
+    service.getAlreadyItems = function functionName() {
+      return alreadyItems ;
+    }
+
+    service.buyItem = function (itemIndex) {
+      
+      var item = addItems[itemIndex];
+      alreadyItems.push(item);
+      addItems.splice(itemIndex,1);
+    }
+
+
+  }
+
+  AddtoList.$inject = ['ShoppingListService'];
+  function AddtoList(ShoppingListService) {
+  var insert = this ;
+}
+
+
+
+  ToBuyController.$inject = ['ShoppingListService'];
+  function ToBuyController(ShoppingListService) {
+    
+  var toBuy = this;
+  toBuy.items = ShoppingListService.getaddItems();
+
+  toBuy.buyItem = function(itemIndex) {
+  ShoppingListService.buyItem(itemIndex);
+  }
+
+  toBuy.errorMessage = function () {
+    if(toBuy.items.length > 0)
+      return false ;
+
+    return true ;
+  }
+
+}
+
+
+
+AlreadyBoughtController.$inject = ['ShoppingListService'];
+function AlreadyBoughtController(ShoppingListService) {
+  var already = this ;
+
+  already.items=ShoppingListService.getAlreadyItems();
+
+  already.errorMessage = function () {
+    if(already.items.length > 0)
+      return false ;
+
+    return true ;
+  }
+}
+    
+    })();
+    
