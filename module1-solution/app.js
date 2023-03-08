27a586d08b8673aef9bccdb9598b88f03e73790c
Yana Borovska <109426911+borovskayana@users.noreply.github.com>
2023-03-08 16:14:05 +0100

    Update app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,42 +1,41 @@
 (function () {
-'use strict';
+    'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+    angular.module('LunchCheck', [])
+        .controller('LunchCheckController', LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController ($scope) {
-    $scope.list = [];
-    
- 
-    
-    $scope.sayM = function() {
-        var words = $scope.list.split(' ');
-        
-    if (words.length <= 3) {
-        $scope.justSay = 'Enjoy';
-    } else if (words.length > 3) {
-        $scope.justSay = 'Too much';
-    } else {
-        $scope.justSay = 'none';
-    } 
-    $scope.fontStyle = {
-        "color":"green"
+    function LunchCheckController($scope) {
+        $scope.empty = "";
+        $scope.justSay = "";
+
+        $scope.checkTooMuch = function () {
+            if ($scope.empty === "") {
+                $scope.justSay = "Please enter data first";
+                $scope.fontStyle = {
+                    "color":"red"
+                    };
+                    $scope.boxStyle = {
+                    "border-color":"red"
+                    };
+                return;
+            }
+            var msg = $scope.empty.split(" ");
+            if (msg.length <= 3) {
+                $scope.justSay = "Enjoy!";
+                $scope.fontStyle = {
+                    "color":"green"
+                    };
+                    $scope.boxStyle = {
+                    "border-color":"green"
+                    };
+            } else $scope.justSay = "Too much!";
+            $scope.fontStyle = {
+                "color":"green"
+                };
+                $scope.boxStyle = {
+                "border-color":"green"
+                };
         };
-        $scope.boxStyle = {
-        "border-color":"green"
-        };
-  
-}
-    
-    
-    };
-    
-
-
-
-
-
-
-    
+    }
 })();
